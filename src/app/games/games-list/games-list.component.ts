dfb7a05a6e2049ea16d964c8ce4de691b54f2b57
Evan Schultz <evan@rangle.io>
2019-02-21 15:28:23 -0500

    basic game list
    * Use the http client to fetch a list of games
    * show use of `*ngFor`
    * show use of async pipe and a loading place holder

--- src/app/games/games-list/games-list.component.ts
+++ src/app/games/games-list/games-list.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { delay } from "rxjs/operators";
+import { Observable } from 'rxjs';
+interface Game {
+  id: number;
+  name: string;
+  image: string;
+  platformId: number;
+  priority: number;
+  numberOfHoursPlayed: number;
+  numberOfHoursToComplete: number;
+  isComplete: false;
+}
 @Component({
-  selector: 'app-games-list',
-  templateUrl: './games-list.component.html',
-  styleUrls: ['./games-list.component.scss']
+  selector: "app-games-list",
+  templateUrl: "./games-list.component.html",
+  styleUrls: ["./games-list.component.scss"]
 })
 export class GamesListComponent implements OnInit {
-
-  constructor() { }
+  games: Game[] = [];
+  games$: Observable<Game[]>;
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
+    this.games$ = this.http
+      .get<Game[]>("http://localhost:3000/games")
+      .pipe<Game[]>(delay(4000));
   }
-
 }
