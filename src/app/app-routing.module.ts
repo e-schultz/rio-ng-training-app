78a964d613d0bfcdb22f02ddb2a126bfc0f2eda2
Evan Schultz <evan@rangle.io>
2019-02-22 12:43:04 -0500

    basic setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PlaceholderComponent } from './shared/placeholder/placeholder.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: PlaceholderComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
