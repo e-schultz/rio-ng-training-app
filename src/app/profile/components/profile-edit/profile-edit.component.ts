3ef9e356aefa77c4f69aa754a65e398de42f3aa1
Evan Schultz <evan@rangle.io>
2019-02-22 20:22:41 -0500

    starting point

--- src/app/profile/components/profile-edit/profile-edit.component.ts
+++ src/app/profile/components/profile-edit/profile-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-edit',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-edit.component.scss']
 })
 export class ProfileEditComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  form: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.form = fb.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      averageNumberOfHoursPerDay: ['', [Validators.min(0), Validators.max(24)]]
+    });
   }
 
+  ngOnInit() {}
 }
--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -5,11 +5,27 @@ import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileHomeComponent } from './components/profile-home/profile-home.component';
 import { ProfileViewComponent } from './components/profile-view/profile-view.component';
 import { ProfileEditComponent } from './components/profile-edit/profile-edit.component';
-import { MatToolbarModule, MatButtonModule, MatCardModule } from '@angular/material';
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatCardModule,
+  MatInputModule,
+  MatFormFieldModule
+} from '@angular/material';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ProfileHomeComponent, ProfileViewComponent, ProfileEditComponent],
-  imports: [CommonModule, MatToolbarModule, ProfileRoutingModule, MatButtonModule, MatCardModule]
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    ProfileRoutingModule,
+    MatButtonModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule
+  ]
 })
 export class ProfileModule {}
